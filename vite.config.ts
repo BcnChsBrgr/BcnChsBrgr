eae1d2f1f7f4d5efde1af057767c576f49cdfe59
BcnChsBrgr <yuhinng41@gmail.com>
2026-03-04 00:38:14 +0000

    update with base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: "/BcnChsBrgr/",
     build: {
         outDir: path.join(__dirname, "docs"),
         emptyOutDir: true,
