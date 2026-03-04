c0da38bc8bbd536b8fd26714c60580e54c62f991
BcnChsBrgr <yuhinng41@gmail.com>
2026-03-04 00:22:54 +0000

    rebuild website

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,9 @@ const App = () => (
             <Sonner />
             <BrowserRouter>
                 <Routes>
-                    <Route path="/BcnChsBrgr" element={<Index />} />
+                    <Route path="/*" element={<Index />} />
                     {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                    <Route path="*" element={<NotFound />} />
+                    {/* <Route path="*" element={<NotFound />} /> */}
                 </Routes>
             </BrowserRouter>
         </TooltipProvider>
